3cd866e3d7097567fe04073c6d496ae823df243f
entirelymagic@gmail.com <@lexandr@123>
2021-01-22 17:50:19 +0200

    Reworked readme, added details

--- data/db.py
+++ data/db.py
@@ -1,5 +1,5 @@
 import pymongo
-
+from time import time
 
 client = pymongo.MongoClient(
     "mongodb+srv://flask_db_user:flask_db_pass@portfolio.bqtz2.mongodb.net/portfolio?retryWrites=true&w=majority"
@@ -12,5 +12,6 @@ def add_new_contact(data):
     db.contact_details.insert({
         'email': data['email'],
         'subject': data['subject'],
-        'message': data['message']
+        'message': data['message'],
+        'date': time()
     })
